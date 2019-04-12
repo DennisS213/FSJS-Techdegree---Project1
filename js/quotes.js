928f3c492e3d6718a1a8de441f52d10ad55e5c10
Dennis <dss213@gmail.com>
2019-04-12 23:21:34 +0200

    FSJS Techdegree - Projekt 1
    Random Quote Generator

---
+++ js/quotes.js
@@ -0,0 +1,34 @@
+// Quote File
+
+var quotes = [
+    {
+        quotation: "Computers themselves, and software yet to be developed, will revolutionize the way we learn.",
+        source: "Steve Jobs",
+        citation: "",
+        year: ""
+    },
+    {
+        quotation: "It's still magic even if you know how it's done",
+        source: "Terry Pratchett",
+        citation: "A Hat Full of Sky",
+        year: ""
+    },
+    {
+        quotation: "Please, no matter how we advance technologically, please don't abandon the book. There is nothing in our material world more beautiful than the book.",
+        source: "Acceptance speech",
+        citation: "National Book Award",
+        year: "2010"
+    },
+    {
+        quotation: "It's supposed to be automatic, but actually you have to push this button.",
+        source: "John Brunner",
+        citation: "Stand on Zanzibar",
+        year: ""
+    },
+    {
+        quotation: "The iPod completely changed the way people approach music.",
+        source: "Karl Lagerfeld",
+        citation: "",
+        year: ""
+    }
+]
